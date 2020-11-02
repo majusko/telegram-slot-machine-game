def48bbba83abc96a5381de18c629be56f221fa5
Mario Kapusta <mariokapustaa@gmail.com>
2020-11-02 22:35:05 +0100

    Remove debug log.

--- start-redis.sh
+++ start-redis.sh
@@ -6,6 +6,6 @@ docker run -d \
   -e REDIS_PASSWORD="$TELEGRAM_BOT_REDIS_PASS" \
   -v slot-machine-volume:/data \
   -p 6379:6379 \
-  --name redis \
+  --name my-redis-container \
   --restart always \
   redis:5.0.5-alpine3.9 /bin/sh -c 'redis-server --appendonly yes --requirepass ${REDIS_PASSWORD}'
