a7ef77114f506389307e7b984301c9e70217c0e3
Mario Kapusta <mariokapustaa@gmail.com>
2020-11-02 17:10:03 +0100

    Use normal redis instead of alpine.

--- start-redis.sh
+++ start-redis.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker volume create slot-machine-volume
-docker create -v slot-machine-volume:/data --name my-redis-container -p 6379:6379 redis:alpine
+docker create -v slot-machine-volume:/data --name my-redis-container -p 6379:6379 dockerfile/redis
 docker start my-redis-container
